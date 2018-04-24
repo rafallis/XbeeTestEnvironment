6f0213617d7c443e525fe0bc42d667b74d962fbf
Rafael Falcão Gomes Jardim <rafaelfgjardim@gmail.com>
2018-04-24 16:41:56 -0300

    First things first

---
+++ main.py
@@ -0,0 +1,68 @@
+import sys
+import serial, glob
+from PyQt5 import QtWidgets, uic
+
+
+class SerialGUI(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super(SerialGUI, self).__init__()
+
+        uic.loadUi('mainwindow.ui', self)
+
+        self.btn_connect.clicked.connect(self.hello)
+        self.btn_refresh.clicked.connect(self.updateSerialPorts)
+        self.list_ports.itemClicked.connect(self.updateSerialConfigurations)
+        self.btn_editPortConfig.clicked.connect(self.editPortConfig)
+        self.btn_savePortConfig.clicked.connect(self.savePortConfig)
+
+        self.show()
+
+    def updateSerialPorts(self):
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i+1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsuported plaftorm')
+
+        self.list_ports.clear()
+
+        for port in ports:
+            self.list_ports.addItem(port)
+
+    def updateSerialConfigurations(self):
+        config = serial.Serial(self.list_ports.currentItem().text())
+        print(config)
+        self.comboBox_baudrate.addItem(str(config.baudrate), 1)
+        self.comboBox_bytesize.addItem(str(config.bytesize), 1)
+        self.comboBox_parity.addItem(str(config.parity), 1)
+        self.comboBox_stopbits.addItem(str(config.stopbits), 1)
+        self.comboBox_timeout.addItem(str(config.timeout), 1)
+        config.close()
+
+    def editPortConfig(self):
+        print("Editando as configurações da porta serial")
+        self.btn_savePortConfig.setEnabled(True)
+
+    def savePortConfig(self):
+        print("Configuração da porta salva")
+        self.btn_savePortConfig.setEnabled(False)
+
+    def serialConnect(self):
+        conn = serial.Serial(self.list_ports.currentItem().text())
+        print(conn)
+
+    def hello(self):
+        print("HELLO")
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = SerialGUI()
+    window.setWindowTitle('Xbee Test Environment')
+    window.updateSerialPorts()
+
+    sys.exit(app.exec_())
